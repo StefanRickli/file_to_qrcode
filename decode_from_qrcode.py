3b3315b349b8dbf7509e057594077306be83342e
Stefan Rickli <stefa@IX.frosted.local>
2021-04-18 02:01:50 +0200

    Detect many errors (mixed up files, hashes, wrong output hash)

--- decode_from_qrcode.py
+++ decode_from_qrcode.py
@@ -1,33 +1,80 @@
-import sys
+import sys, traceback
 import os
 import glob
+import json
+import base64
+import hashlib
 
-file_list = glob.glob('./decode/in/*.txt')
-newest_file = max(file_list, key=os.path.getctime)
+in_folder = './decode/in'
+out_folder = './decode/out'
+
+file_list = glob.glob(f'{in_folder}/*.txt')
+try:
+    newest_file = max(file_list, key=os.path.getmtime)
+except ValueError:
+    print(f'Could not find a file to process in "{in_folder}"')
+    sys.exit(1)
+print(f'Stitching together "{newest_file}"')
 
 try:
     f = open(newest_file, 'r')
 except Exception as e:
-    print(e)
+    traceback.print_exc()
     sys.exit(1)
 
 raw = f.read()
 f.close()
 
-data_dict = {}
+got_chunk = False
+chunk_data_arr = {}
+chunk_numbers = []
 
-chunks = raw.replace('\n', '').split('|')
-for chunk in chunks:
-    if '?' in chunk and ':' in chunk:
-        filename, chunk_rest = chunk.split('?')
-        chunk_number, chunk_data = chunk_rest.split(':')
-        print(filename, chunk_number)
-        data_dict[f'{filename}_{chunk_number}'] = chunk_data
+code_contents = raw.splitlines()
+for i, code_content in enumerate(code_contents):
+    # print(code_content)
+    try:
+        chunk = json.loads(code_content)
+        if got_chunk:
+            if file_name != chunk['file_name']:
+                raise ValueError('File name mismatch')
+            if file_hash != chunk['file_sha256']:
+                raise ValueError('File hash mismatch')
+            if chunks_total != int(chunk['chunks_total']):
+                raise ValueError('Chunk total mismatch')
+        else:
+            got_chunk = True
+            print(f"Original file name: {chunk['file_name']}")
+            print(f"Original file hash (SHA256):  {chunk['file_sha256']}")
+            print(f"Total chunks to process: {chunk['chunks_total']}")
+            file_name = chunk['file_name']
+            file_hash = chunk['file_sha256']
+            chunks_total = int(chunk['chunks_total'])
+        
+        print(f"Found chunk #{chunk['chunk_idx']}")
 
-keys = sorted(data_dict.keys())
-data = [data_dict[k] for k in keys]
-txt = ''.join(data)
+        chunk_data_arr[f"{chunk['file_name']}_{chunk['chunk_idx']}"] = chunk['data']
+        chunk_numbers.append(int(chunk['chunk_idx']))
+    except json.JSONDecodeError as e:
+        pass
 
-f = open(f'./decode/out/{filename}', 'w')
-f.write(txt)
+if not all(i in chunk_numbers for i in range(1, chunks_total+1)):
+    raise ValueError('Missing at least one chunk')
+
+keys = sorted(chunk_data_arr.keys())
+chunk_data_sorted = [chunk_data_arr[k] for k in keys]
+data_b64txt = ''.join(chunk_data_sorted)
+data = base64.b64decode(data_b64txt.encode())
+
+hash_object = hashlib.sha256(data)
+data_hash = hash_object.hexdigest()
+print(f'Decoded data hash: {data_hash}')
+if data_hash != file_hash:
+    raise ValueError('Decoded data hash does not match with original hash')
+print('Hashes match.')
+
+out_file = os.path.join(out_folder, file_name)
+f = open(f'{out_file}', 'wb')
+f.write(data)
 f.close()
+
+print(f'Restored file to "{out_file}"')
--- encode_to_qrcode.py
+++ encode_to_qrcode.py
@@ -1,19 +1,25 @@
 import sys
 import os
 import glob, ntpath, posixpath
+import math
+import base64
+import json
+import hashlib
 import treepoem
 
-chunk_size = 180
+chunk_size = 400
+qr_code_eclevel = 'Q'  # all options are [L, M, Q, H]
 
 file_list = glob.glob('./encode/in/*.txt')
-newest_file = max(file_list, key=os.path.getctime)
+newest_file = max(file_list, key=os.path.getmtime)
+print(f'Converting {newest_file} to QR codes')
 if os.name == 'nt':
     newest_file_name = ntpath.basename(newest_file)
 else:
     newest_file_name = posixpath.basename(newest_file)
 
 try:
-    f = open(newest_file, 'r')
+    f = open(newest_file, 'rb')
 except Exception as e:
     print(e)
     sys.exit(1)
@@ -21,8 +27,26 @@ except Exception as e:
 data = f.read()
 f.close()
 
-chunks = [data[i:i+chunk_size] for i in range(0, len(data), chunk_size)]
+hash_object = hashlib.sha256(data)
+data_hash = hash_object.hexdigest()
+data_b64txt = base64.b64encode(data).decode()
+chunk_data_arr = [data_b64txt[i:i+chunk_size] for i in range(0, len(data_b64txt), chunk_size)]
 
-for i, chunk in enumerate(chunks):
-    image = treepoem.generate_barcode('qrcode', f'{newest_file_name}?{i:02}:' + chunk + '|', {})
-    image.save(f'./encode/out/{newest_file_name} ({i:02}).png')
+chunks_total = len(chunk_data_arr)
+n_digits = math.ceil(chunks_total/10)
+pad_fmt = f'0{n_digits}'
+for i, chunk_data in enumerate(chunk_data_arr):
+    chunk = {
+        'file_name': f'{newest_file_name}',
+        'file_sha256': data_hash,
+        'chunk_idx': f'{i+1:{pad_fmt}}',
+        'chunks_total': f'{chunks_total:{pad_fmt}}',
+        'data': chunk_data
+    }
+
+    code_content = json.dumps(chunk)
+
+    image = treepoem.generate_barcode('qrcode', code_content, {'eclevel': qr_code_eclevel})
+    out_filename = f'./encode/out/{newest_file_name} ({i+1:{pad_fmt}} of {chunks_total:{pad_fmt}}).png'
+    print(out_filename)
+    image.save(out_filename)
