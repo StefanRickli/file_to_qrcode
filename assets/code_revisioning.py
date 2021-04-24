2315908f4bd90f8763cd298524f67d3ad6383054
Stefan Rickli <stefanrickli@gmx.ch>
2021-04-24 22:14:44 +0200

    Convert backup_to_qrcode.py to CLI

--- assets/code_revisioning.py
+++ assets/code_revisioning.py
@@ -2,4 +2,4 @@
 # This file is generated automatically by the Git pre-commit hook.
 
 def get_software_timestamp():
-    return '2021-04-23 11:11:45'
+    return '2021-04-24 22:14:44'
--- backup_to_qrcode.py
+++ backup_to_qrcode.py
@@ -2,16 +2,13 @@
 import sys
 import os
 from datetime import datetime
-import glob
-import ntpath
-import posixpath
-from pathlib import Path
 import math
 import base64
 import json
 import hashlib
 import logging
 import uuid
+import argparse
 
 # Other modules
 import treepoem
@@ -20,61 +17,12 @@ import treepoem
 import generate_pdf
 from assets import code_revisioning
 
-
+# TODO put into argparse
 chunk_size = 400
 qr_code_eclevel = 'M'  # all options are [L, M, Q, H]
-out_folder = './backup/out'
-
-experiment_number = 0
-while True:
-    log_filename = f'{out_folder}/{experiment_number:03}.log'
-    if os.path.exists(log_filename):
-        experiment_number += 1
-    else:
-        break
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(message)s",
-    handlers=[
-        logging.FileHandler(log_filename),
-        logging.StreamHandler()
-    ]
-)
 
 
-file_list = glob.glob('./backup/in/*')
-newest_file = max(file_list, key=os.path.getmtime)
-print(f'Converting {newest_file} to QR codes')
-if os.name == 'nt':
-    input_file_path = ntpath.basename(newest_file)
-else:
-    input_file_path = posixpath.basename(newest_file)
-input_file_name = Path(input_file_path).name
-input_file_name_b64txt = base64.b32encode(input_file_name.encode()).decode()
-
-try:
-    f = open(newest_file, 'rb')
-except Exception as e:
-    print(e)
-    sys.exit(1)
-
-data = f.read()
-f.close()
-
-software_timestamp = code_revisioning.get_software_timestamp()
-
-hash_object = hashlib.sha256(data)
-data_sha256 = hash_object.hexdigest().upper()
-data_b32txt = base64.b32encode(data).decode()
-chunk_data_arr = [data_b32txt[i:i + chunk_size] for i in range(0, len(data_b32txt), chunk_size)]
-
-chunks_total = len(chunk_data_arr) + 1  # +1 for the metadata chunk
-n_digits = math.ceil(math.log10(chunks_total))
-pad_fmt = f'0{n_digits}'
-batch_timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-batch_uuid = uuid.uuid4().hex.upper()
-
+# -----------------------------------------------------------------------------------
 
 def write_qr_code(chunk_idx, chunk_content, out_file_path):
     qr_code_content = json.dumps(chunk_content)
@@ -99,12 +47,85 @@ def write_qr_code(chunk_idx, chunk_content, out_file_path):
     image.save(out_file_path)
 
 
+# -----------------------------------------------------------------------------------
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', '--source', type=str, required=True)
+parser.add_argument('-d', '--destination', type=str, required=True)
+parser.add_argument('-l', '--logfile', type=str)
+parser.add_argument('--preserve_tempfiles', type=bool, default=False)
+
+args = parser.parse_args()
+
+in_file_path = args.source
+in_file_basename = os.path.basename(in_file_path)
+
+dest_path = args.destination
+if os.path.basename(dest_path):
+    if os.path.splitext(args.destination)[1].lower() != '.pdf':
+        raise ValueError('Unknown destination file extension. Need ".pdf" or ".PDF".')
+    run_pdf_generation = True
+    out_file_path = dest_path
+    out_file_basename = os.path.basename(dest_path)
+    out_folder = os.path.dirname(dest_path)
+else:
+    run_pdf_generation = False
+    out_file_path = dest_path
+    out_file_basename = in_file_basename
+    out_folder = dest_path
+
+if args.logfile is None:
+    log_file_path = f'{os.path.join(out_folder, out_file_basename)}.log'
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(message)s",
+    handlers=[
+        logging.FileHandler(log_file_path),
+        logging.StreamHandler()
+    ]
+)
+
+logging.info(f'Converting "{in_file_path}" to "{out_file_path}"')
+
+# -----------------------------------------------------------------------------------
+
+# #################
+# Read input file #
+# #################
+
+try:
+    f = open(in_file_path, 'rb')
+    data = f.read()
+    f.close()
+except Exception as e:
+    logging.info(e)
+    sys.exit(1)
+
+# ######################
+# Assemble (meta-)data #
+# ######################
+
+software_timestamp = code_revisioning.get_software_timestamp()
+
+in_file_basename_b64txt = base64.b32encode(in_file_basename.encode()).decode()
+
+data_sha256 = hashlib.sha256(data).hexdigest().upper()
+data_b32txt = base64.b32encode(data).decode()
+chunk_data_arr = [data_b32txt[i:i + chunk_size] for i in range(0, len(data_b32txt), chunk_size)]
+
+chunks_total = len(chunk_data_arr) + 1  # +1 for the metadata chunk
+n_digits = math.ceil(math.log10(chunks_total))
+pad_fmt = f'0{n_digits}'
+batch_timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+batch_uuid = uuid.uuid4().hex.upper()
+
 # ################
 # Metadata Chunk #
 # ################
 
 batch_metadata = {'software_timestamp': software_timestamp,
-                  'file_name': input_file_name,
+                  'file_name': in_file_basename,
                   'file_sha256': data_sha256,
                   'batch_timestamp': batch_timestamp,
                   'batch_id': batch_uuid,
@@ -118,7 +139,7 @@ batch_metadata = {'software_timestamp': software_timestamp,
 # for an explanation of the capacity of a QR code with
 # respect to ECL, mode and version.
 batch_metadata_qr = {'SOFTWARE_TIMESTAMP': software_timestamp,
-                     'FILE_NAME': input_file_name_b64txt,
+                     'FILE_NAME': in_file_basename_b64txt,
                      'FILE_SHA256': data_sha256,
                      'BATCH_TIMESTAMP': batch_timestamp,
                      'BATCH_ID': batch_uuid,
@@ -126,11 +147,11 @@ batch_metadata_qr = {'SOFTWARE_TIMESTAMP': software_timestamp,
                      'CHUNK_IDX': f'{1:{pad_fmt}}'
                      }
 
-out_file_path = f'{out_folder}/{input_file_name} ({1:{pad_fmt}} Metadata).png'
-write_qr_code(1, batch_metadata_qr, out_file_path)
+img_file_path = f'{out_folder}/{in_file_basename} ({1:{pad_fmt}} Metadata).png'
+write_qr_code(1, batch_metadata_qr, img_file_path)
 
 image_files = []
-image_files.append({'chunk_idx': '1', 'chunk_img_path': out_file_path})
+image_files.append({'chunk_idx': '1', 'chunk_img_path': img_file_path})
 
 # #############
 # Data Chunks #
@@ -144,18 +165,18 @@ for i, chunk_data in enumerate(chunk_data_arr):
         'DATA': chunk_data
     }
 
-    out_file_path = f'{out_folder}/{input_file_name} ({i:{pad_fmt}} of {chunks_total:{pad_fmt}}).png'
-    write_qr_code(i, chunk_content, out_file_path)
+    img_file_path = f'{out_folder}/{out_file_basename} ({i:{pad_fmt}} of {chunks_total:{pad_fmt}}).png'
+    write_qr_code(i, chunk_content, img_file_path)
 
     image_files.append({'chunk_idx': i,
-                        'chunk_img_path': out_file_path
+                        'chunk_img_path': img_file_path
                         })
 
 # -----------------------------------------------------------------------------------
 
-print('Generating PDF...')
-in_data = {'meta': batch_metadata, 'image_files': image_files}
-pdf_file_path = f'{out_folder}/{input_file_name}.pdf'
-generate_pdf.run(in_data, pdf_file_path)
-print(f'File written to "{pdf_file_path}".')
-print('Done!')
+if run_pdf_generation:
+    logging.info('Generating PDF...')
+    in_data = {'meta': batch_metadata, 'image_files': image_files}
+    generate_pdf.run(in_data, out_file_path)
+    logging.info(f'File written to "{out_file_path}".')
+    logging.info('Done!')
