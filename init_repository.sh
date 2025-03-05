9efc9e100f218190e2b31f839f05a1d9885aad27
Keysie <keysie@keysie-system.ch>
2025-03-05 23:43:08 +0100

    Add some quality of life to repo - add a requirements.txt with the current python package versions - add virtual environment creation to initialization script - add automatic install of requirements to init script - include venv in .gitignore

--- init_repository.sh
+++ init_repository.sh
@@ -2,8 +2,21 @@
 
 set -e
 
-echo Setting up Git pre-commit hook...
+echo -n Setting up Git pre-commit hook...
 
 cp ./assets/pre-commit ./.git/hooks/pre-commit
+
+echo Done.
+
+echo -n Setting up virtual environment...
+
+python3 -m venv venv
+
+echo Done.
+
+echo Install requirements locally...
+
+source ./venv/bin/activate
+pip install -r requirements.txt
 
 echo Done.
